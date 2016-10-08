039c2b4f5ef3ddb8bab05d93cbf7cf503a457789
pavel <birladeanupavel@gmail.com>
2016-10-08 13:38:14 +0300

    Added log4j2 logging

--- src/main/java/com/birladeanu/AuctionApplication.java
+++ src/main/java/com/birladeanu/AuctionApplication.java
@@ -1,12 +1,16 @@
 package com.birladeanu;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuctionApplication {
+	private static Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
+		logger.debug("@@@@@@@@@TESTTTT");
 		SpringApplication.run(AuctionApplication.class, args);
 	}
 }
